b5fc941aba657af2d0f4306721a22e01ea27a82a
SamuelDino <samuel_qualitats@hotmail.com>
2022-01-22 00:48:57 -0300

    second commit

--- src/main/java/com/example/languagecourseclient/LanguageCourseClientApplication.java
+++ src/main/java/com/example/languagecourseclient/LanguageCourseClientApplication.java
@@ -1,18 +1,10 @@
 package com.example.languagecourseclient;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @SpringBootApplication
 public class LanguageCourseClientApplication {
--- src/main/java/com/example/languagecourseclient/controller/ClientController.java
+++ src/main/java/com/example/languagecourseclient/controller/ClientController.java
@@ -2,6 +2,8 @@ package com.example.languagecourseclient.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +12,11 @@ import org.springframework.web.client.RestTemplate;
 import java.util.List;
 
 @RestController
+@RefreshScope
 public class ClientController {
 
     @Autowired
+    @Lazy
     private RestTemplate template;
 
     @Value("${languagecourse.provider.url}")
